7347a6da938a65401b11ff80ab407abbf0f0c6e6
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-03-20 21:54:01 -0500

    useEffect Warnings

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,11 +1,22 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({ x: 0, y: 0 });
+    const { x, y } = coords;
+
     useEffect(() => {
-        console.log('componente montado');
+
+        const mouseMove = (e) => {
+            const coords = { x: e.x, y: e.y };
+            setCoords( coords );
+        }
+
+        window.addEventListener('mousemove', mouseMove);
+
+
         return () => {
-            console.log('componente desmontado');
+            window.removeEventListener('mousemove', mouseMove);
         };
     }, []);
 
@@ -13,6 +24,9 @@ export const Message = () => {
     return (
         <div>
             <h3>Awesome!!</h3>
+            <p>
+                x: { x } y: { y }
+            </p>
         </div>
     );
 };
