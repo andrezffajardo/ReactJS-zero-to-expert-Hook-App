7ade64c3808aa3d5195d7ad6f635958d1ce5ddb2
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-03-21 22:36:09 -0500

    Form With Custom Hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export const useForm = (initState = {} ) => {
+
+    const [values, setValues] = useState(initState);
+
+    const handleInputChange = ({ target }) => {
+        setValues({
+            ...values,
+            [ target.name ]: target.value
+        });
+    }
+
+    return [ values, handleInputChange ];
+
+
+};
