efe6b32d1cdf9376da76a45ccc8897e48596a799
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-03-30 22:18:29 -0500

    Delete a TODO

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -20,6 +20,16 @@ export const TodoApp = () => {
         localStorage.setItem('todos', JSON.stringify( todos ) )
     }, [todos]);
 
+    const handleDelete = ( todoId ) => {
+
+        const action = {
+            type: 'delete',
+            payload: todoId
+        }
+
+        dispatch( action );
+    }
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -62,8 +72,9 @@ export const TodoApp = () => {
                                     <p className="text-center"> { i + 1 }. { todo.desc } </p>
                                     <button
                                         className="btn btn-danger"
+                                        onClick={ () => handleDelete( todo.id ) }
                                     >
-                                        Erase
+                                        Delete
                                     </button>
                                 </li>
                             ))
