99ee10b88eca0d6707a7f9c7da2de6e29af0dc25
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-03-31 22:23:08 -0500

    Toggle Todo - mark as completed or pending a TODO

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -30,6 +30,16 @@ export const TodoApp = () => {
         dispatch( action );
     }
 
+    const handleToggle = ( todoId ) => {
+
+        dispatch({
+            type: 'toggle',
+            payload: todoId
+        });
+    }
+
+
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -69,7 +79,12 @@ export const TodoApp = () => {
                                     key={ todo.id }
                                     className="list-group-item"
                                 >
-                                    <p className="text-center"> { i + 1 }. { todo.desc } </p>
+                                    <p
+                                        className={ `${ todo.done && 'complete' }` }
+                                        onClick={ () => handleToggle( todo.id )}
+                                    >
+                                        { i + 1 }. { todo.desc }
+                                    </p>
                                     <button
                                         className="btn btn-danger"
                                         onClick={ () => handleDelete( todo.id ) }
