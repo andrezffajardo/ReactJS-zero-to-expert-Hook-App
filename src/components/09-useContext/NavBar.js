2567ed3098a33fb967a44afefbae43490ae5b2bc
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-04-07 18:43:53 -0500

    Link and NavLink correction navbar

--- src/components/09-useContext/NavBar.js
+++ src/components/09-useContext/NavBar.js
@@ -4,10 +4,15 @@ import {Link, NavLink} from 'react-router-dom';
 export const NavBar = () => {
     const isActiveStyle = (navData) => navData.isActive ? "nav-link active" : "nav-link"
     return (
-        <ul className="nav nav-tabs nav-fill navbar navbar-light justify-content-center">
-            <li className='nav-item'><NavLink className={isActiveStyle} to="/">Home</NavLink></li>
-            <li className='nav-item'><NavLink className={isActiveStyle} to="/about">About</NavLink></li>
-            <li className='nav-item'><NavLink className={isActiveStyle} to="/login">Login</NavLink></li>
-        </ul>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <Link to="/" className="navbar-brand">useContext</Link>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+                <NavLink className={isActiveStyle} to="/">Home</NavLink>
+                <NavLink className={isActiveStyle} to="/about">About</NavLink>
+                <NavLink className={isActiveStyle} to="/login">Login</NavLink>
+            </div>
+        </div>
+    </nav>
     );
 };
