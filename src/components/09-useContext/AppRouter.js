c7d05dd048f5a97fa24ced4d3212db37d6c1bbed
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-04-08 16:14:46 -0500

    useContext Example

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -15,12 +15,14 @@ export const AppRouter = () => {
         <Router>
             <div>
                 <NavBar />
-                <Routes>
-                    <Route path="/" element={ <HomeScreen/> } />
-                    <Route path="/about" element={ <AboutScreen/> } />
-                    <Route path="/login" element={ <LoginScreen/> } />
-                    <Route path="*" element={ <Navigate to="/"/> } />
-                </Routes>
+                <div className="container">
+                    <Routes>
+                        <Route path="/" element={ <HomeScreen/> } />
+                        <Route path="/about" element={ <AboutScreen/> } />
+                        <Route path="/login" element={ <LoginScreen/> } />
+                        <Route path="*" element={ <Navigate to="/"/> } />
+                    </Routes>
+                </div>
             </div>
         </Router>
 
